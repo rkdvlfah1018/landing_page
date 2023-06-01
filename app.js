6372330a5b5b166c03eb3e72dc1c6c2d764e7fd9
William Kang <wkang2020@gmail.com>
2023-05-31 21:22:22 -0500

    made multiple pages to fit 80 percent at a time

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-const navDiv = document.querySelector('.nav');
-const  infoDiv= document.querySelector('.info');
+document.addEventListener('DOMContentLoaded', function(){
+    const navDiv = document.querySelector('div.nav');
+    const navLinks = navDiv.querySelectorAll('a');
+    const sections = document.querySelectorAll('section');
 
-// Get the width of the sibling div
-const navWidth = navDiv.offsetWidth + 'px';
+    navLinks.forEach(function(link){
+        link.addEventListener('click', function(e){
+            e.preventDefault();
+            const target = document.querySelector(this.getAttribute('href'));
+            scrollToSection(target);
+        });
+    });
 
-// Set the CSS variable value
-infoDiv.style.setProperty('--navDiv-width', navDiv);
+    function scrollToSection(target){
+        sections.forEach(function(section){
+            section.classList.remove('active');
+        });
+
+        target.classList.add('active');
+    }
+});
